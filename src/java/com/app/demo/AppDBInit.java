36b35f6258b863c8e943c399aea2593c11736281
Prasanna <amruta@hp>
2017-04-27 23:29:03 +0530

    Updated.

--- src/java/com/app/demo/AppDBInit.java
+++ src/java/com/app/demo/AppDBInit.java
@@ -10,25 +10,32 @@ import java.sql.SQLException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
 /**
  *
  * @author Prasanna
  */
+@WebListener
 public class AppDBInit implements ServletContextListener {
      
-    final String DBURL = "jdbc:postgresql://localhost:5433/SweetyDB?user=postgres&password=postgres";
+    //final String DBURL = "jdbc:postgresql://localhost:5433/SweetyDB?user=postgres&password=postgres";
     JdbcPooledConnectionSource source=  null;
     public void contextInitialized(ServletContextEvent event){
         /*
         Initialize ORMLIte connection. Currently only one DB connection
-        */
-        
+        */        
         try{
-          source = new JdbcPooledConnectionSource(DBURL); 
+            String DBUrl = event.getServletContext().getInitParameter("DBUrl");
+            String DBUserName = event.getServletContext().getInitParameter("DBUserName");
+            String DBPassword = event.getServletContext().getInitParameter("DBPassword");
+            DBUrl = DBUrl.concat("?user=").concat(DBUserName).concat("password=").concat(DBPassword);
+          source = new JdbcPooledConnectionSource(DBUrl); 
           //keep connection open for 30 minutes.
           source.setMaxConnectionAgeMillis(30*60*1000);
+          System.out.println("---------------------------------------");
           System.out.println("Connection pool with SweetyDB created");
+          System.out.println("----------------------------------------");
           
         }
         catch(SQLException sql){
--- src/java/com/app/demo/LoginServlet.java
+++ src/java/com/app/demo/LoginServlet.java
@@ -30,16 +30,7 @@ import javax.servlet.http.HttpSession;
 @WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    private JdbcPooledConnectionSource conn;
+   private JdbcPooledConnectionSource conn;
     @Override
     public void init(ServletConfig config){
         try{
